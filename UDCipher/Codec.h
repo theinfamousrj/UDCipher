54426a0bdd3e08a7d601e9ac1313e0948ff79d83
RJ Hill <infamous.rj@gmail.com>
2012-01-26 23:07:07 -0500

    initial commit: README

---
+++ UDCipher/Codec.h
@@ -0,0 +1,13 @@
+//
+//  Codec.h
+//  UDCipher
+//
+//  Created by theinfamousrj on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Codec : NSObject
+
+@end
---
+++ UDCipher/Codec.m
@@ -0,0 +1,13 @@
+//
+//  Codec.m
+//  UDCipher
+//
+//  Created by theinfamousrj on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Codec.h"
+
+@implementation Codec
+
+@end
